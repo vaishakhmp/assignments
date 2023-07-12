51d3226c30f4b5a05d9ade39e424f8d3398f32ca
vaishakhmp <125654696+vaishakhmp@users.noreply.github.com>
2023-07-12 17:28:54 +0530

    new assignment completed

---
+++ test6.js
@@ -0,0 +1,15 @@
+// Assume the product prices and quantities
+var price1 = 10;
+var quantity1 = 2;
+
+var price2 = 5;
+var quantity2 = 4;
+
+var price3 = 8;
+var quantity3 = 3;
+
+// Calculate the total items in the cart
+var totalItems = quantity1 + quantity2 + quantity3;
+
+// Print the total items in the cart
+console.log("Total Items: " + totalItems);
